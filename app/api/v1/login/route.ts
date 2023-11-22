b3fd1b1a6a45de846d490fb5010c197f2afdf6fb
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-11-22 17:47:02 +0800

    [login] Set login backend and connect to front end

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { pool } from "../../../(db_related)/configure_db";
 
 export async function GET(){
@@ -6,6 +6,12 @@ export async function GET(){
     return new NextResponse(JSON.stringify(data))
 }
 
-export async function POST(){
-    return new NextResponse('Post')
+export async function POST(request: NextRequest){
+    let request_json = await request.json()
+
+    // TODO: set HTTP 403 when username or password invalid
+    let data: Array<Map<string, string>> = await pool.query(
+        "SELECT username FROM credential WHERE username = ? AND password = ?", 
+        [request_json['username'], request_json['password']]) ?? []
+    return new NextResponse(JSON.stringify(data))
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,50 @@
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-    title: 'Login Page',
-    description: 'Halaman login',
-  }
+"use client"
+import { redirect} from "next/navigation";
+import { useState, useEffect} from "react";
 
 export default function LoginPage(){
+    const [loginInfo, setLoginInfo] = useState({})
+    const [isAuthenticated, setAuthenticated] = useState(false)
+
+    useEffect(() => {
+        if (isAuthenticated){
+            redirect('/dashboard')
+        }
+    }, [isAuthenticated])
+
+    function handleLogin(event: React.ChangeEvent<HTMLInputElement>){
+        
+        setLoginInfo(prev => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    function handleSubmit(){
+        const redirectPage = (isAuthenticated: boolean) => {
+            redirect('/dashboard')
+        }
+
+        const fetchLogin = async () => {
+            console.log(process.env.APP_HOST)
+            let response = await fetch(
+                `http://localhost:3000/api/v1/login`, {
+                    method: "POST",
+                    body: JSON.stringify(loginInfo),
+                    credentials: "include" // need to set because CORS
+                })
+            if (response.ok){
+                // TODO: set cookie to not back 
+                // again to login page if cookie is valid
+                let data = await response.json() ?? []
+                if (data?.length !== 0){
+                    setAuthenticated(true)
+                }
+            }
+        }
+        fetchLogin()
+    }
+
     return (
         <div className="flex flex-col items-center h-screen justify-center">
             <div className="flex flex-col border-4 p-2 rounded">
@@ -14,15 +53,15 @@ export default function LoginPage(){
                 </div>
                 <div className="flex flex-col p-1">
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" className="border-2 py-1 px-2 rounded" />
+                    <input type="text" name="username" id="username" className="border-2 py-1 px-2 rounded" onChange={handleLogin}/>
                 </div>
 
                 <div className="flex flex-col p-1 mb-1">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" className="border-2 py-1 px-2 rounded"/>
+                    <input type="password" name="password" id="password" className="border-2 py-1 px-2 rounded" onChange={handleLogin}/>
                 </div>
                 <div className="flex flex-col items-center">
-                    <button type="button" className="bg-sky-400 p-2 rounded text-white">Login</button>
+                    <button type="button" className="bg-sky-400 p-2 rounded text-white" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
