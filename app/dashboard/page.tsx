b93f6ee81dcec92f82b1d248b93bfb70a3c37c50
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-11-24 09:35:56 +0800

    [dashboard] Add product request functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 
 function DashboardSection({title, children}:{title: string, children: React.ReactNode}){
     return (
@@ -19,9 +19,22 @@ type ProductDataType = {
     'quantity': number
 }
 
+type ProductDataRequestType = {
+    'id': number,
+    'name': string,
+    'quantity': number,
+    'type': 'add' | 'delete' | 'update'
+}
+
 export default function DashboardHomePage(){
     const [isAddDialogVisible, setAddDialogVisible] = useState(false)
     const [productData, setProductData] = useState<Array<ProductDataType>>([])
+    const [addProductInfo, setAddProductInfo] = useState<ProductDataRequestType>({
+        id: -1,
+        name: 'dummy',
+        quantity: -1,
+        type: 'add'
+    })
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -33,6 +46,24 @@ export default function DashboardHomePage(){
         }
         fetchProduct()
     }, [])
+
+    function handleAddProductRequest(){
+        const user = 'user'
+        const requestAddProduct = async () => {
+            await fetch('http://localhost:3000/api/v1/product_request', {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify({
+                    'product_id': addProductInfo.id,
+                    'product_name': addProductInfo.name,
+                    'product_quantity': addProductInfo.quantity,
+                    'type': 'add',
+                    'username': user
+                })
+            })
+        }
+        requestAddProduct()
+    }
 
     return (
         <section className="flex flex-grow flex-col">
@@ -61,19 +92,34 @@ export default function DashboardHomePage(){
                         <div className="border p-2">
                         <div className="flex flex-col">
                                 <label htmlFor="product_id_add" className="capitalize">ID</label>
-                                <input type="text" name="product_id_add" id="product_id_add" className="border rounded p-1"/>
+                                <input type="text" name="product_id_add" id="product_id_add" className="border rounded p-1" 
+                                    onChange={(event) => setAddProductInfo({
+                                        ...addProductInfo,
+                                        'id': Number(event.target.value)
+                                    })}/>
                             </div>
                             <div className="flex flex-col">
                                 <label htmlFor="name" className="capitalize">name</label>
-                                <input type="text" name="name" id="name" className="border rounded p-1"/>
+                                <input type="text" name="name" id="name" className="border rounded p-1"
+                                    onChange={(event) => setAddProductInfo({
+                                        ...addProductInfo,
+                                        'name': event.target.value
+                                    }
+                                )}/>
                             </div>
                             <div className="flex flex-col">
                                 <label htmlFor="quantity" className="capitalize">quantity</label>
-                                <input type="number" name="quantity" id="quantity" className="border rounded p-1"/>
+                                <input type="number" name="quantity" id="quantity" className="border rounded p-1"
+                                    onChange={(event) => setAddProductInfo({
+                                        ...addProductInfo,
+                                        'quantity': Number(event.target.value)
+                                    }
+                                )}/>
                             </div>
 
                             <div className="flex justify-evenly my-1">
                                 <button className="bg-green-300 p-1 rounded" onClick={() => {
+                                    handleAddProductRequest()
                                     setAddDialogVisible(false)
                                 }}>Add</button>
                                 <button className="bg-red-500 p-1 rounded" onClick={() => {
