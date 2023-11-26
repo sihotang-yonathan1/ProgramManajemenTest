9fbaf7dc1c6110e5eb977fee93e58f0094091d7d
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-11-26 20:07:28 +0800

    [api/login] Give appropriate response when invalid or server is nonactive

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { pool } from "../../../(db_related)/configure_db";
+import { SqlError } from "mariadb";
 
 export async function GET(){
     let data = await pool.query("SELECT username FROM credential");
@@ -10,8 +11,26 @@ export async function POST(request: NextRequest){
     let request_json = await request.json()
 
     // TODO: set HTTP 403 when username or password invalid
-    let data: Array<Map<string, string>> = await pool.query(
-        "SELECT username FROM credential WHERE username = ? AND password = ?", 
-        [request_json['username'], request_json['password']]) ?? []
-    return new NextResponse(JSON.stringify(data))
+    try {
+        let data: Array<Map<string, string>> = await pool.query(
+            "SELECT username FROM credential WHERE username = ? AND password = ?", 
+            [request_json['username'], request_json['password']]) ?? []
+            return new NextResponse(JSON.stringify(data))
+    } catch (error) {
+        let data;
+        let response_status = {
+            'http_code': 403
+        }
+        if (error instanceof SqlError){
+            data = {'success': false, 'error': {
+                'sql_state': error.sqlState,
+                'isFatal': error.fatal,
+                'error_number': error.errno
+            }
+        }
+        return new NextResponse(JSON.stringify(data || {}), {
+            status: response_status.http_code
+        })
+        }
+    }
 }
