9946e8bfb5db57acf2cc656b54b3cf41054de06d
Yonathan Sihotang <yonathansihotang026@student.unsrat.ac.id>
2023-11-27 10:22:31 +0800

    [api/product] Change to Prisma

--- app/api/v1/product/route.ts
+++ app/api/v1/product/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from "next/server";
-import { pool } from "../../../(db_related)/configure_db";
+import { prisma } from "../../../(db_related)/configure_db";
 
 // TODO: check auth
 export async function GET(){
-    let data = await pool.query("SELECT id, name, quantity FROM product") ?? [];
+    // let data = await pool.query("SELECT id, name, quantity FROM product") ?? [];
+    await prisma.$connect()
+    let data = await prisma.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            quantity: true
+        }
+    })
+    await prisma.$disconnect()
     return new NextResponse(JSON.stringify(data));
 }
